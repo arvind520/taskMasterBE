c6384bb0bf3eb2a4cc7923fef45d51c9b950e3ff
Arvind Chauhan <40683239+arvind520@users.noreply.github.com>
2024-01-11 10:47:25 +0530

    Update conn.js
    added db url

--- conn/conn.js
+++ conn/conn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const conn = async (req, res) => {
   try {
     await mongoose
-      .connect("db url")
+      .connect("mongodb+srv://arvindgta:agta1234@cluster0.ngyriot.mongodb.net/")
       .then(() => console.log("DB connection successfull!"));
   } catch (error) {
     console.log("error", error)
