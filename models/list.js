a1dc13ba86b6158cf1ffcb75a4a768c831328630
Arvind Chauhan <40683239+arvind520@users.noreply.github.com>
2024-01-13 17:01:08 +0530

    Update list.js
    removed title and body required

--- models/list.js
+++ models/list.js
@@ -4,11 +4,9 @@ const listSchema = mongoose.Schema(
   {
     title: {
       type: String,
-      required: true,
     },
     body: {
       type: String,
-      required: true,
     },
     user: [
       {
